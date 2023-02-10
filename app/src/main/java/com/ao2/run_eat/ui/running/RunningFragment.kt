0135d23bc0f3a998054d06ebc1f93f64c67b097d
JunJangE <wnswkd486@gmail.com>
2023-02-10 16:03:28 +0900

    feat: google map 현재 위치 기능 추가

--- app/src/main/java/com/ao2/run_eat/ui/running/RunningFragment.kt
+++ app/src/main/java/com/ao2/run_eat/ui/running/RunningFragment.kt
@@ -1,15 +1,22 @@
 package com.ao2.run_eat.ui.running
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.viewModels
 import com.ao2.run_eat.R
 import com.ao2.run_eat.base.BaseFragment
 import com.ao2.run_eat.databinding.FragmentRunningBinding
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,10 +28,15 @@ class RunningFragment : BaseFragment<FragmentRunningBinding, RunningViewModel>(R
     private val TAG = "RunningFragment"
     // GoogleMap - 기본 지도 기능 및 데이터를 관리하기 위한 진입점
     private lateinit var mMap: MapView
+    private lateinit var googleMap: GoogleMap
     override val layoutResourceId: Int
         get() = R.layout.fragment_running
 
     override val viewModel : RunningViewModel by viewModels()
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
+    lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
+    internal lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는
+    private val REQUEST_PERMISSION_LOCATION = 10
 
     override fun initStartView() {
         binding.apply {
@@ -32,9 +44,23 @@ class RunningFragment : BaseFragment<FragmentRunningBinding, RunningViewModel>(R
             this.lifecycleOwner = viewLifecycleOwner
         }
         exception = viewModel.errorEvent
+
+        mLocationRequest =  LocationRequest.create().apply {
+
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        }
+
+        startLocationUpdates()
         mMap = binding.googleMapView
         mMap.onCreate(Bundle())
         mMap.getMapAsync(this)
+
+
+        binding.btnCurrentPosition.setOnClickListener {
+            startLocationUpdates()
+            Log.d("Ttt", "클릭됨")
+        }
 
     }
 
@@ -45,13 +71,42 @@ class RunningFragment : BaseFragment<FragmentRunningBinding, RunningViewModel>(R
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-
+        this.googleMap = googleMap
         // 초기 위치 설정 및 마커 표시
-        val latLng = LatLng(37.566168, 126.901609)
-        googleMap.addMarker(MarkerOptions().position(latLng).title("여기"))
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+
     }
 
+    private fun startLocationUpdates() {
+
+        //FusedLocationProviderClient의 인스턴스를 생성.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(requireActivity(),Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("ttt", "ddd")
+            return
+        }
+        // 기기의 위치에 관한 정기 업데이트를 요청하는 메서드 실행
+        // 지정한 루퍼 스레드(Looper.myLooper())에서 콜백(mLocationCallback)으로 위치 업데이트를 요청
+        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    // 시스템으로 부터 위치 정보를 콜백으로 받음
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            // 시스템에서 받은 location 정보를 onLocationChanged()에 전달
+            locationResult.lastLocation
+            onLocationChanged(locationResult.lastLocation!!)
+        }
+    }
+    // 시스템으로 부터 받은 위치정보를 화면에 갱신해주는 메소드
+    fun onLocationChanged(location: Location) {
+        mLastLocation = location
+        Log.d("ttt mLastLocation", mLastLocation.toString())
+        val latLng = LatLng(mLastLocation.latitude, mLastLocation.longitude)
+        googleMap.addMarker(MarkerOptions().position(latLng).title("여기"))
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+
+    }
 
     override fun onStart() {
         super.onStart()
