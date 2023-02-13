b108cb46921a899425bb345a74fbba803968c84b
junjange <wnswkd486@naver.com>
2023-02-13 15:03:20 +0900

    feat: navigation ui 구현

--- app/src/main/java/com/ao2/run_eat/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ao2/run_eat/ui/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.ao2.run_eat.ui.home
 
+import android.animation.ObjectAnimator
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.ao2.run_eat.R
 import com.ao2.run_eat.base.BaseFragment
 import com.ao2.run_eat.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 
 @AndroidEntryPoint
@@ -23,12 +27,102 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
             this.lifecycleOwner = viewLifecycleOwner
         }
         exception = viewModel.errorEvent
+        setupEvent()
+    }
+
+    private fun setupEvent() {
+        lifecycleScope.launchWhenStarted {
+//            viewModel.navigationEvent.collect {
+//                when(it) {
+//                    is HomeNavigationAction.NavigateToInit -> Unit
+//                    is HomeNavigationAction.NavigateToCreate -> navigate(HomeFragmentDirections.actionHomeFragmentToCreateTodoFragment())
+//                    is HomeNavigationAction.NavigateToTodoDetail -> navigate(HomeFragmentDirections.actionHomeFragmentToDetailFragment(it.todoIdx))
+//                    is HomeNavigationAction.NavigateToPaging -> navigate(HomeFragmentDirections.actionHomeFragmentToPagingFragment())
+//                }
+//            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.toggleStateEvent.collectLatest {
+                toggleFab(it)
+            }
+        }
     }
 
     override fun initDataBinding() {
     }
 
     override fun initAfterBinding() {
+    }
+
+    private fun toggleFab(state: Boolean) {
+        // 플로팅 액션 버튼 닫기 - 열려있는 플로팅 버튼 집어넣는 애니메이션
+        if (!state) {
+            ObjectAnimator.ofFloat(binding.btAddTodo, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btAddTodo, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtAddTodo, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtAddTodo, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToPaging, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToPaging, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToPaging, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToPaging, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btDeleteTodo, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btDeleteTodo, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtDeleteTodo, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtDeleteTodo, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToShare, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToShare, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToShare, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToShare, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToBag, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToBag , "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToBag, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToBag , "translationX", 0f).apply { start() }
+
+
+            ObjectAnimator.ofFloat(binding.btTodo, View.ROTATION, 45f, 0f).apply { start() }
+        } else { // 플로팅 액션 버튼 열기 - 닫혀있는 플로팅 버튼 꺼내는 애니메이션
+
+            ObjectAnimator.ofFloat(binding.btToShare, "translationY", -360f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToShare, "translationX", +180f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToShare, "translationY", -360f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToShare, "translationX", +180f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToBag, "translationY", -120f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToBag, "translationX", +270f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToBag, "translationY", -120f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToBag, "translationX", +270f).apply { start() }
+
+
+            ObjectAnimator.ofFloat(binding.btToPaging, "translationY", -500f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToPaging, "translationY", -500f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btAddTodo, "translationY", -360f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btAddTodo, "translationX", -180f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtAddTodo, "translationY", -360f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtAddTodo, "translationX", -180f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btDeleteTodo, "translationY", -120f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btDeleteTodo, "translationX", -270f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtDeleteTodo, "translationY", -120f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtDeleteTodo, "translationX", -270f).apply { start() }
+
+
+            ObjectAnimator.ofFloat(binding.btTodo, View.ROTATION, 0f, 45f).apply { start() }
+        }
     }
 
 
--- app/src/main/java/com/ao2/run_eat/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ao2/run_eat/ui/home/HomeViewModel.kt
@@ -8,8 +8,19 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-) : BaseViewModel() {
+) : BaseViewModel(), HomeActionHandler {
+    var toggleStateEvent: MutableStateFlow<Boolean> = MutableStateFlow(false)
 
     private val TAG = "HomeViewModel"
+
+    private fun whenToggleState() {
+        if(toggleStateEvent.value) {
+            toggleStateEvent.value = !toggleStateEvent.value
+        }
+    }
+
+    override fun onToggleFab() {
+        toggleStateEvent.value = !toggleStateEvent.value
+    }
 
 }
