2b1ac54414d49cec083f192350a9252f4e6749de
junjange <wnswkd486@naver.com>
2023-02-14 14:15:42 +0900

    feat: data class Inventory 생성

---
+++ domain/src/main/java/com/ao2/domain/model/Inventory.kt
@@ -0,0 +1,9 @@
+package com.ao2.domain.model
+
+data class Inventory(
+    val food_id: Int,
+    val title: String,
+    val description: String,
+    val food_img_path: String,
+    val food_count: Int,
+)
