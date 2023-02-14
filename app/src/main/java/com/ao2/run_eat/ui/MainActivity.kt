14b3753ffa8d648b86e87556295e38d9409b828b
junjange <wnswkd486@naver.com>
2023-02-14 11:17:22 +0900

    feat: bottom navigation 기능 main에 추가

--- app/src/main/java/com/ao2/run_eat/ui/MainActivity.kt
+++ app/src/main/java/com/ao2/run_eat/ui/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.ao2.run_eat.ui
 
+import android.animation.ObjectAnimator
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.ao2.run_eat.NavigationGraphDirections
 import com.ao2.run_eat.R
 import com.ao2.run_eat.base.BaseActivity
 import com.ao2.run_eat.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
@@ -25,15 +28,158 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     var waitTime = 0L
 
     override fun initStartView() {
+        binding.apply {
+            this.vm = viewModel
+        }
+
+        initNavController()
 
         /** DynamicLink 수신확인 */
         initDynamicLink()
+
     }
 
     override fun initDataBinding() {
     }
 
     override fun initAfterBinding() {
+    }
+
+    private fun initNavController() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> showBottomNav()
+                R.id.RunningFragment -> showBottomNav()
+                R.id.myPageFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+
+        setupEvent()
+
+
+//        binding.btTodo.setupWithNavController(navController)
+//
+//        // 중복터치 막기!!
+//        binding.btTodo.setOnItemReselectedListener { menuItem ->
+//            when (menuItem.itemId) {
+//                R.id.homeFragment -> {}
+//                R.id.alarmRoomTabFragment -> {}
+//                R.id.myPageFragment -> {}
+//            }
+//        }
+    }
+
+    private fun setupEvent() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.navigationHandler.collect {
+                Log.d("ttt", "1")
+
+                when (it) {
+//                    is HomeNavigationAction.NavigateToInit -> Unit
+                    is MainNavigationAction.NavigateToRunning -> navController.navigate(R.id.RunningFragment)
+//                    is HomeNavigationAction.NavigateToCreate -> navigate(HomeFragmentDirections.actionHomeFragmentToCreateTodoFragment())
+//                    is HomeNavigationAction.NavigateToTodoDetail -> navigate(HomeFragmentDirections.actionHomeFragmentToDetailFragment(it.todoIdx))
+//                    is HomeNavigationAction.NavigateToPaging -> navigate(HomeFragmentDirections.actionHomeFragmentToPagingFragment())
+                    else -> {}
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.toggleStateEvent.collectLatest {
+                toggleFab(it)
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.btTodo.visibility = View.VISIBLE
+        binding.btAddTodo.visibility = View.VISIBLE
+        binding.btDeleteTodo.visibility = View.VISIBLE
+        binding.btToBag.visibility = View.VISIBLE
+        binding.btToPaging.visibility = View.VISIBLE
+        binding.btToShare.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.btTodo.visibility = View.GONE
+        binding.btAddTodo.visibility = View.GONE
+        binding.btDeleteTodo.visibility = View.GONE
+        binding.btToBag.visibility = View.GONE
+        binding.btToPaging.visibility = View.GONE
+        binding.btToShare.visibility = View.GONE
+    }
+
+    private fun toggleFab(state: Boolean) {
+        // 플로팅 액션 버튼 닫기 - 열려있는 플로팅 버튼 집어넣는 애니메이션
+        if (!state) {
+            ObjectAnimator.ofFloat(binding.btAddTodo, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btAddTodo, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtAddTodo, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtAddTodo, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToPaging, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToPaging, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToPaging, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToPaging, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btDeleteTodo, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btDeleteTodo, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtDeleteTodo, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtDeleteTodo, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToShare, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToShare, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToShare, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToShare, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToBag, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToBag, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToBag, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToBag, "translationX", 0f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btTodo, View.ROTATION, 45f, 0f).apply { start() }
+
+        } else { // 플로팅 액션 버튼 열기 - 닫혀있는 플로팅 버튼 꺼내는 애니메이션
+            ObjectAnimator.ofFloat(binding.btToShare, "translationY", -360f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToShare, "translationX", +180f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToShare, "translationY", -360f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToShare, "translationX", +180f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToBag, "translationY", -120f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btToBag, "translationX", +270f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtToBag, "translationY", -120f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToBag, "translationX", +270f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btToPaging, "translationY", -500f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtToPaging, "translationY", -500f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btAddTodo, "translationY", -360f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btAddTodo, "translationX", -180f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtAddTodo, "translationY", -360f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtAddTodo, "translationX", -180f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btDeleteTodo, "translationY", -120f).apply { start() }
+            ObjectAnimator.ofFloat(binding.btDeleteTodo, "translationX", -270f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.txtDeleteTodo, "translationY", -120f).apply { start() }
+            ObjectAnimator.ofFloat(binding.txtDeleteTodo, "translationX", -270f).apply { start() }
+
+            ObjectAnimator.ofFloat(binding.btTodo, View.ROTATION, 0f, 45f).apply { start() }
+        }
     }
 
     override fun onBackPressed() {
@@ -43,9 +189,13 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
             } else {
                 when (navController.currentDestination?.id) {
                     R.id.homeFragment -> {
-                        if(System.currentTimeMillis()-waitTime >= 1500) {
+                        if (System.currentTimeMillis() - waitTime >= 1500) {
                             waitTime = System.currentTimeMillis()
-                            Toast.makeText(this, getString(R.string.onBackPressed_Message), Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this,
+                                getString(R.string.onBackPressed_Message),
+                                Toast.LENGTH_SHORT
+                            ).show()
                         } else {
                             finishAffinity() // 액티비티 종료
                         }
--- app/src/main/java/com/ao2/run_eat/ui/MainViewModel.kt
+++ app/src/main/java/com/ao2/run_eat/ui/MainViewModel.kt
@@ -2,10 +2,38 @@ package com.ao2.run_eat.ui
 
 import com.ao2.run_eat.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : BaseViewModel() {
+class MainViewModel @Inject constructor() : BaseViewModel(), MainActionHandler {
 
     private val TAG = "MainViewModel"
+
+    var toggleStateEvent: MutableStateFlow<Boolean> = MutableStateFlow(false)
+
+
+    private val _navigationHandler: MutableSharedFlow<MainNavigationAction> = MutableSharedFlow<MainNavigationAction>()
+    val navigationHandler: SharedFlow<MainNavigationAction> = _navigationHandler.asSharedFlow()
+
+    private fun whenToggleState() {
+        if(toggleStateEvent.value) {
+            toggleStateEvent.value = !toggleStateEvent.value
+        }
+    }
+
+    override fun onToggleFab() {
+        toggleStateEvent.value = !toggleStateEvent.value
+    }
+
+    override fun onToggleRunningClicked() {
+        baseViewModelScope.launch {
+            _navigationHandler.emit(MainNavigationAction.NavigateToRunning)
+            whenToggleState()
+        }
+    }
 }
