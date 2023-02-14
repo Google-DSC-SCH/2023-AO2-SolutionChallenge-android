05e8d349bddffff7bb988eb9faa3b44ada6cb649
junjange <wnswkd486@naver.com>
2023-02-14 15:20:01 +0900

    feat: 인벤토리 페이징 처리

--- app/src/main/java/com/ao2/run_eat/ui/inventory/InventoryFragment.kt
+++ app/src/main/java/com/ao2/run_eat/ui/inventory/InventoryFragment.kt
@@ -1,22 +1,26 @@
 package com.ao2.run_eat.ui.inventory
 
+import android.util.Log
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.ao2.run_eat.R
 import com.ao2.run_eat.base.BaseFragment
-import com.ao2.run_eat.databinding.FragmentHomeBinding
 import com.ao2.run_eat.databinding.FragmentInventoryBinding
+import com.ao2.run_eat.ui.inventory.adapter.InventoryAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 
 @AndroidEntryPoint
 class InventoryFragment : BaseFragment<FragmentInventoryBinding, InventoryViewModel>(R.layout.fragment_inventory) {
 
-    private val TAG = "HomeFragment"
+    private val TAG = "InventoryFragment"
 
     override val layoutResourceId: Int
         get() = R.layout.fragment_inventory
 
     override val viewModel: InventoryViewModel by viewModels()
+    private val inventoryAdapter by lazy { InventoryAdapter(viewModel) }
 
     override fun initStartView() {
         binding.apply {
@@ -34,6 +38,17 @@ class InventoryFragment : BaseFragment<FragmentInventoryBinding, InventoryViewMo
     }
 
     override fun initAfterBinding() {
+        binding.rvInventoryList.adapter = inventoryAdapter
+
+    }
+
+    private fun initAdapter() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.inventoryList.collectLatest {
+                Log.d("ttt 알람 히스토리 데이터 변경", "알람 히스토리 데이터 변경")
+                inventoryAdapter.submitData(it)
+            }
+        }
     }
 
 }
--- app/src/main/java/com/ao2/run_eat/ui/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/ao2/run_eat/ui/inventory/InventoryViewModel.kt
@@ -1,20 +1,36 @@
 package com.ao2.run_eat.ui.inventory
 
+import androidx.paging.PagingData
+import com.ao2.domain.model.Inventory
+import com.ao2.domain.repository.MainRepository
 import com.ao2.run_eat.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class InventoryViewModel @Inject constructor() : BaseViewModel() {
+class InventoryViewModel @Inject constructor(
+    private val mainRepository: MainRepository
+) : BaseViewModel(), InventoryActionHandler {
 
     private val TAG = "InventoryViewModel"
 
     private val _navigationHandler: MutableSharedFlow<InventoryNavigationAction> = MutableSharedFlow<InventoryNavigationAction>()
     val navigationHandler: SharedFlow<InventoryNavigationAction> = _navigationHandler.asSharedFlow()
+
+    var inventoryList: Flow<PagingData<Inventory>> = emptyFlow()
+    var groupId = MutableStateFlow<Int>(0)
+    var sort = MutableStateFlow<String>("")
+
+
+    fun getInventory() {
+//        inventoryList = createInventoryPager(
+//            mainRepository = mainRepository,
+//            groupId = groupId,
+//            sort = sort,
+//            viewModel = this
+//        ).flow.cachedIn(baseViewModelScope)
+    }
 
 }
