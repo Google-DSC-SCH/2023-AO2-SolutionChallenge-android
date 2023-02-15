8d8ac71dea78c0a96cf1c86562da58bcb2c3b04e
junjange <wnswkd486@naver.com>
2023-02-15 16:19:36 +0900

    feat: SettingFragment 생성

---
+++ app/src/main/java/com/ao2/run_eat/ui/setting/SettingFragment.kt
@@ -0,0 +1,38 @@
+package com.ao2.run_eat.ui.setting
+
+import androidx.fragment.app.viewModels
+import com.ao2.run_eat.R
+import com.ao2.run_eat.base.BaseFragment
+import com.ao2.run_eat.databinding.FragmentSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class SettingFragment : BaseFragment<FragmentSettingBinding, SettingViewModel>(R.layout.fragment_setting) {
+
+    private val TAG = "SettingFragment"
+
+    override val layoutResourceId: Int
+        get() = R.layout.fragment_setting
+
+    override val viewModel: SettingViewModel by viewModels()
+
+    override fun initStartView() {
+        binding.apply {
+            this.viewmodel = viewModel
+            this.lifecycleOwner = viewLifecycleOwner
+        }
+        exception = viewModel.errorEvent
+        setupEvent()
+    }
+
+    private fun setupEvent() {
+    }
+
+    override fun initDataBinding() {
+    }
+
+    override fun initAfterBinding() {
+    }
+
+}
