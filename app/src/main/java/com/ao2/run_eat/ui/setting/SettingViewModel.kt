5cd530c43032f7e588cbf5df26085621cf856864
junjange <wnswkd486@naver.com>
2023-02-15 16:19:55 +0900

    feat: SettingViewModel 생성

---
+++ app/src/main/java/com/ao2/run_eat/ui/setting/SettingViewModel.kt
@@ -0,0 +1,13 @@
+package com.ao2.run_eat.ui.setting
+
+import com.ao2.run_eat.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(
+) : BaseViewModel() {
+
+    private val TAG = "SettingViewModel"
+
+}
