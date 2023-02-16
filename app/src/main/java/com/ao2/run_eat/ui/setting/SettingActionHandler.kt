450402da52b9970f328b24b232e0d230cacc0ee7
junjange <wnswkd486@naver.com>
2023-02-16 14:45:43 +0900

    feat: SettingActionHandler추가

--- app/src/main/java/com/ao2/run_eat/ui/setting/SettingActionHandler.kt
+++ app/src/main/java/com/ao2/run_eat/ui/setting/SettingActionHandler.kt
@@ -2,4 +2,7 @@ package com.ao2.run_eat.ui.setting
 
 
 interface SettingActionHandler {
+    fun onNewPushToggled(checked: Boolean)
+    fun onReactionPushToggled(checked: Boolean)
+    fun onNotReceivedPushAtNight(checked: Boolean)
 }
